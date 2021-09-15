8c908f593ed726b26cba40a977c6d2881bc4664b
dipicakrishna <66508946+dipicakrishna@users.noreply.github.com>
2021-09-15 15:33:16 +0530

    first commit

--- GitHubReposJavaProject/src/com/lti/demo/UserApp.java
+++ GitHubReposJavaProject/src/com/lti/demo/UserApp.java
@@ -4,6 +4,8 @@ public class UserApp {
 public static void main(String[]args) {
 	Product p= new Product(101,"Mobile",5000);
 	System.out.println(p);
+
 	System.out.println("New line added");
+	System.out.println("New line added2");
 }
 }
